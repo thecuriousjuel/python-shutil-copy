021d148a4e825af397534280f601b05547be1f69
Biswajit Basak <juelbasak@gmail.com>
2023-03-04 13:54:18 +0530

    Script changed to generate files

--- generate_text_files.py
+++ generate_text_files.py
@@ -5,9 +5,10 @@ import os
 now = datetime.now()
 
 for i in range(300):
-    now.strftime('%d%m%Y')
-    now += timedelta(days=1)
     date = now.strftime('%d%m%Y')
+
     filename = date + '.txt'
     with open(os.path.join('data', filename), mode='w') as fp:
         fp.write(f'This is file {filename}')
+
+    now += timedelta(days=1)
